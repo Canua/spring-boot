19f8c3051c66afece82ea4e9430c965c517310fa
choigisuk2 <choigisuk2@naver.com>
2019-03-25 14:24:55 +0900

    수업

--- spring-boot-mysite/src/main/java/com/douzone/mysite/BootApplication.java
+++ spring-boot-mysite/src/main/java/com/douzone/mysite/BootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 위 어노테이션을 대체
 @SpringBootApplication
 public class BootApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
 	}
